4aad8d8323d7d8a2845b7268ef4659ea013f15b6
Bruse zero <zero8297183@163.com>
2018-04-15 17:32:37 +0800

    修改体现页面js

--- public/javascripts/public.js
+++ public/javascripts/public.js
@@ -149,6 +149,7 @@ var getLocaltime = function (_time) {
  * 仿JQ的fade方法显示、隐藏函数
  * @param {对象} obj.elem 作用的元素
  *               obj.handle 隐藏(hide)或显示(show)
+ *               obj.time 隐藏(hide)或显示(show)的时间
  * 调用方法：
  * 建议元素（obj.elem）使用 class ，因为后面用id的样式去覆盖（实现过渡），
  * 如果elem 使用 id, 那被替换后原来的样式将 丢 失！
@@ -158,6 +159,7 @@ var getLocaltime = function (_time) {
 var fadeFn = function(obj){
     // 默认显示
     obj.handle = obj.handle || 'show';
+    obj.time = obj.time || '500';
 
     if(obj.handle == 'show'){
         // 设置显示的id
@@ -169,7 +171,7 @@ var fadeFn = function(obj){
         // 延时隐藏（恢复隐藏状态）
         setTimeout(function(){
             obj.elem.setAttribute('id', '');
-        },500);
+        },obj.time);
     }
 }
 
