9813639b6ae6fed800992315d4abfad02e336246
zero9527 <zero8297183@163.com>
2018-05-07 18:40:05 +0800

    修改图片上传函数

--- views/resource/javascripts/upImg.js
+++ views/resource/javascripts/upImg.js
@@ -1,7 +1,43 @@
 /**
  * [upFile 图片上传]
  * @param {[type]} [formdata] [formdata]
+ * @param {[type]} [input] [input type="file"的元素]
  * @param {[function]} [callback] [回调函数]
+ * 用法：
+ * 	html: +++++++++++++++
+ * 	<form action="" id='form'>
+        <input type="file" id='file' multiple="">
+        <div id='picShow'></div>
+    </form>
+    
+    js: +++++++++++++++++
+ *  var form = document.querySelector('#form');
+    var file = document.querySelector('#file');
+    var picShow = document.querySelector('#picShow');
+    file.onclick = function(){
+        var formdata = new FormData(form);
+        var _this = this;
+
+        // 读取图片
+        upImg(formdata, _this, function(res){
+            console.log('res: ',res);
+
+            //图片显示
+            var span = document.createElement('span');
+            var spanclose = document.createElement('span');
+            spanclose.setAttribute('class', 'delPic');
+            spanclose.setAttribute('style', 'z-index:999;');
+            span.appendChild(spanclose);
+            var img = new Image();
+            
+            if(res.src){
+                img.src = res.src;      // 显示图片
+                span.appendChild(img);
+                picShow.appendChild(span);
+                // console.log(formdata.getAll('UploadForm[]'));
+            }
+        })
+    }
  */
 var upImg = function(formdata, input, callback){
 	formdata = formdata ? formdata : new FormData();
@@ -13,13 +49,17 @@ var upImg = function(formdata, input, callback){
 		var fileList = e.target.files;
 		console.log('原始： ',fileList)
 		if(fileList.length == 1){
+			// 单选
 			var reader = new FileReader();
 			// 读文件
 			reader.readAsDataURL(fileList[0]);
-			reader.onload = function(){
-				readerFun(this);
+			reader.onload = function(e){
+				// console.log('e1: ',e);
+				readerFun(e, this);
 			}
 		}else if(fileList.length >= 2){
+			// 多选 input multiple
+			//（ PC可以，安卓因为不支持多选所以只有一张会显示, ios未测试）
 			// console.log(Object.prototype.toString.call(fileList))
 			for(var i in fileList){
 				// console.log(Object.prototype.toString.call(fileList[i]));
@@ -28,8 +68,9 @@ var upImg = function(formdata, input, callback){
 					(function(file){
 
 						reader.readAsDataURL(file);
-						reader.onload = function(){
-							readerFun(this);
+						reader.onload = function(e){
+							// console.log('e2: ',e);
+							readerFun(e, this);
 						}
 					})(fileList[i]);
 				}
@@ -42,25 +83,28 @@ var upImg = function(formdata, input, callback){
 	 * @param  {[type]} e [description]
 	 * @return {[type]}   [description]
 	 */
-	function readerFun(e){
-		var result = e.result;
+	function readerFun(e, file_reader){
 		var dx = (e.total/1024)/1024;
+		var result = file_reader.result;
+		console.log('e: ',e);
 		if(dx >= .3){
-			console.log("文件大小大于1M");
-			//文件大于1M则压缩后上传
-			// img[0].src = compressImage(result);
-			backdata['src'] = compressImage(result);
-			backdata['compress'] = true;
-				
+			console.log("文件大小大于600kb");
+			//文件大于600kb则压缩后上传
+			compressImage(result, function(res){
+				backdata['src'] = res,src;
+				backdata['compress'] = true;
+			});
+			
 			// 回调
 			callback(backdata);
 		}else{
 			backdata['src'] = result;
-			// img[0].src = result;
-			
-			//将图片转成二进制后 append 进 formdata
-			formdata.append('UploadForm[]',convert2Binary(result));
-			backdata['compress'] = false;
+			// 将图片转成二进制
+			convert2Binary(result, function(res){
+				//将图片二进制数据 append 进 formdata
+				formdata.append('UploadForm[]', res.src);
+				backdata['compress'] = false;
+			})
 			// 回调
 			callback(backdata);
 			// console.log('不压缩： ',formdata.getAll('UploadForm[]'))
@@ -71,7 +115,7 @@ var upImg = function(formdata, input, callback){
 	/*
 		图片压缩(canvas)
 	 */
-	function compressImage(_url) {
+	function compressImage(_url, callback) {
 		var canvas = document.createElement("canvas");
 		var ctx = canvas.getContext("2d");
 		var img = new Image();
@@ -81,15 +125,20 @@ var upImg = function(formdata, input, callback){
 			canvas.width = img.width*8;
 			canvas.height = img.height*8;
 
+			ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
 			setTimeout(function(){
-				ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-				// img.src = canvas.toDataURL('image/jpeg', .5);
+				img.src = canvas.toDataURL('image/jpeg', .5);
 				// console.log(img.src);
-				//将base64字符串转为2进制
-				formdata.append('UploadForm[]',convert2Binary(img.src));
-				//停止 img.onload
-				img.onload = null;
-				return canvas.toDataURL('image/jpeg', .5);
+				img.onload = function(){
+					//将base64字符串转为2进制
+					convert2Binary(img.src, function(res){
+						formdata.append('UploadForm[]',res.src);
+
+						//停止 img.onload
+						img.onload = null;
+						callback({src: res.src});
+					})
+				}
 			},0);
 		}
 		
@@ -97,7 +146,7 @@ var upImg = function(formdata, input, callback){
 	/*
 		将base64字符串转换为2进制
 	 */
-	function convert2Binary(dataURI){
+	function convert2Binary(dataURI, callback){
 		let byteString = window.atob(dataURI.split(',')[1]);
 		let ab = new ArrayBuffer(byteString.length);
 		let ua = new Uint8Array(ab);
@@ -108,55 +157,8 @@ var upImg = function(formdata, input, callback){
 		// console.log(ua)
 		let bb = new window.Blob([ ab ]);
 		// console.log(bb)
-		return bb;
+		// 回调
+		callback({src: bb});
 	}
-
-	// upFile(formdata, function(res){
-	// 	console.log(res);
-
-	// 	if(res.src){
-	// 		img.src = res.src;		// 显示图片
-	// 		spanclose.addClass("delPic");
-	// 		spanclose.css({zIndex: '999'});
-	// 		span.append(spanclose);
-	// 		span.append(img[0]);
-	// 		$('#picShow').append(span);
-	// 	}
-	// 	//最多3张图片
-	// 	if($('#picShow>span').length > 2){
-	// 		console.log('3张了');
-	// 		$(".file_upload").css({display: 'none'});
-	// 	}else{
-	// 		$(".file_upload").css({display: 'block'});
-	// 	}
-
-	// 	/**
-	// 		点击上传的图片右上角的" x "，删除这张图片
-	// 	 */ 
-	// 	$("#picShow").on("click", ".delPic", function(){
-	// 		// console.log($(this).parent());
-	// 		$(this).parent().remove()
-	// 		//最多3张图片
-	// 		$(".file_upload").css({display: 'block'});
-	// 		var imgUpload = formdata.getAll('UploadForm[]');
-	// 		var index = Number($(this).attr('index'));		//第几张图片
-			
-	// 		// console.log('before: ',imgUpload);
-	// 		//	清空formData数据
-	// 		formdata.delete('UploadForm[]');
-
-	// 		//删除当前元素在formData中的数据
-	// 		imgUpload.splice(index,1);
-	// 		// console.log(Object.prototype.toString.call(imgUpload[index]));
-	// 		// console.log('after: ',imgUpload);
-			
-	// 		//重新将图片数据添加到formData中
-	// 		for(var i=0; i<imgUpload.length; i++){
-	// 			formdata.append('UploadForm[]', imgUpload[i])
-	// 		}
-	// 		console.log('after: ',formdata.getAll('UploadForm[]'));
-
-	// 	})
-	// });
 
 }
